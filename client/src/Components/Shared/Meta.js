ceb4c5295919b12a8d4eac2f64de3a85dfde771b
Patrick James <pjmorgan716@gmail.com>
2019-04-02 00:29:58 -0400

    Revert "Authentication"

--- client/src/Components/Shared/Meta.js
+++
@@ -1,5 +0,0 @@
-export const Meta = {
-    title: "Press-On Nails",
-    description: "High quality hand made press on nails.",
-    keywords: "press ons, press on nails, press on nailz, pierced nails, pierced nailz, nails, nailz, beauty, rave, rave nails, rave nailz"
-}
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import ScrollToTop from '../src/components/ScrollToTop'
-import App from './components/App/App'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDom.render(
-    <Router>
-        <ScrollToTop>
-            <App />
-        </ScrollToTop>
-    </Router>,
-    document.getElementById("root")
-)
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
