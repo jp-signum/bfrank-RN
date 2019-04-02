e33e374b3fac0d2bf079725b488901235b5df49a
Patrick <pjmorgan716@gmail.com>
2019-04-02 00:59:48 -0400

    lets try again

---
+++ client/src/Components/App/Contact/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FixedContainer = styled.div`
+   
+`
+
+function Footer() {
+   
+    return (
+        <FixedContainer>
+            
+        </FixedContainer>
+    )
+}
+
+export default Footer;
