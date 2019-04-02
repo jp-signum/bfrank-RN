da0236b330fc778aff6fe85bccc465a61d99b96a
Patrick <pjmorgan716@gmail.com>
2019-04-02 05:42:00 -0400

    added scroll to top back

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from '/components/App/App'
+import ScrollToTop from './components/ScrollToTop'
+import App from './components/App/App'
 
 ReactDom.render(
     <Router>
-        <App />
+        <ScrollToTop>
+            <App />
+        </ScrollToTop>
     </Router>,
     document.getElementById("root")
 )
