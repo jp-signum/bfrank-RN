fccec20809b4a699dda54e31c68e3dafc98f7e5a
Patrick <pjmorgan716@gmail.com>
2019-04-04 01:51:31 -0400

    google analytics

--- client/src/index.js
+++ client/src/index.js
@@ -1,38 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom';
-import ReactGA from 'react-ga';
-import { PropTypes } from 'prop-types';
 
 import ScrollToTop from './components/ScrollToTop'
 import App from './components/App/App'
-
-const GA_KEY = 'UA-122359368-3';
-
-ReactGA.initialize(GA_KEY);
-
-class GAListener extends React.Component {
-    static contextTypes = {
-        router: PropTypes.object
-    };
-
-    componentDidMount() {
-        this.sendPageView(this.context.router.history.location);
-        this.context.router.history.listen(this.sendPageView);
-    }
-
-    sendPageView(location) {
-        ReactGA.set({ page: location.pathname });
-        ReactGA.pageview(location.pathname);
-    }
-
-    render() {
-        return this.props.children;
-    }
-}
+import GA from './utils/GoogleAnalytics'
 
 ReactDom.render(
     <Router>
+        { GA.init() && <GA.RouteTracker /> }
         <ScrollToTop>
             <App />
         </ScrollToTop>
