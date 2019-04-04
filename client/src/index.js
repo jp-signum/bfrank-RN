6eb1e2a79ad4c1b7e40f23d146d59bded72cd684
Patrick <pjmorgan716@gmail.com>
2019-04-03 23:28:00 -0400

    added google analytic tracking HOC inside of BrowserRouter

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,43 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import { PropTypes } from 'prop-types';
 
 import ScrollToTop from './components/ScrollToTop'
 import App from './components/App/App'
 
+const GA_KEY = 'UA-122359368-3'; 
+
+ReactGA.initialize(GA_KEY);
+
+class GAListener extends React.Component {
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    componentDidMount() {
+        this.sendPageView(this.context.router.history.location);
+        this.context.router.history.listen(this.sendPageView);
+    }
+
+    sendPageView(location) {
+        ReactGA.set({ page: location.pathname });
+        ReactGA.pageview(location.pathname);
+    }
+
+    render() {
+        return this.props.children;
+    }
+}
+
 ReactDom.render(
     <Router>
-        <ScrollToTop>
-            <App />
-        </ScrollToTop>
+        <GAListener>
+            <ScrollToTop>
+                <App />
+            </ScrollToTop>
+        </GAListener>
     </Router>,
     document.getElementById("root")
 )
---
+++ client/src/withTracker.js
@@ -0,0 +1,35 @@
+import ReactGA from 'react-ga';
+import { PropTypes } from 'prop-types';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactGA.initialize(GA_KEY);
+
+class GAListener extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  componentDidMount() {
+    this.sendPageView(this.context.router.history.location);
+    this.context.router.history.listen(this.sendPageView);
+  }
+
+  sendPageView(location) {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+// then wrap GAListener one level inside BrowserRouter
+const App = ({ classes }) => (
+  <BrowserRouter>
+    <GAListener>
+      ...
+    </GAListener>
+  </BrowserRouter>
+);
+
