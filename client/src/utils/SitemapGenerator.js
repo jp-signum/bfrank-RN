26abac4039d4d1b6784ee2339316f74fc108c204
Patrick <pjmorgan716@gmail.com>
2019-08-21 17:43:16 -0400

    first

--- client/src/utils/SitemapGenerator.js
+++ client/src/utils/SitemapGenerator.js
@@ -1,5 +1,5 @@
-require("babel-register")({
-    presets: ["es2015", "react"]
+require('babel-register')({
+    presets: ['es2015', 'react']
 });
 
 const router = require('./SitemapRoutes').default;
